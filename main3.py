628b97796ed862c0bdc284096000557e3d5467e4
Matalin Racster <matalinracster@149-161-135-184.dhcp-bl.indiana.edu>
2019-08-30 16:34:26 -0400

    edit main3

--- main3.py
+++ main3.py
@@ -10,28 +10,28 @@ arcade.set_background_color(open_color.white)
 # Start the render process. This must be done before any drawing commands.
 arcade.start_render()
 
-face_x,face_y = (0,0)
-smile_x,smile_y = (face_x + 0,face_y - 0)
-eye1_x,eye1_y = (face_x - 0,face_y + 0) 
-eye2_x,eye2_y = (face_x + 0,face_y + 0)
-catch1_x,catch1_y = (face_x - 0,face_y + 0) 
-catch2_x,catch2_y = (face_x + 0,face_y + 0) 
+face_x,face_y = (400,300)
+smile_x,smile_y = (face_x + 0,face_y - 50)
+eye1_x,eye1_y = (face_x - 50,face_y + 50) 
+eye2_x,eye2_y = (face_x + 50,face_y + 50)
+catch1_x,catch1_y = (face_x - 40,face_y + 60) 
+catch2_x,catch2_y = (face_x + 60,face_y + 60) 
 
 
 # Draw the smiley face:
 # (x,y,radius,color)
-arcade.draw_circle_filled(face_x, face_y, 100, open_color.yellow_3)
+arcade.draw_circle_filled(face_x, face_y, 200, open_color.yellow_3)
 # (x,y,radius,color,border_thickness)
-arcade.draw_circle_outline(face_x, face_y, 100, open_color.black,4)
+arcade.draw_circle_outline(face_x, face_y, 200, open_color.black,4)
 
 #(x,y,width,height,color)
-arcade.draw_ellipse_filled(eye1_x,eye1_y,15,25,open_color.black)
-arcade.draw_ellipse_filled(eye2_x,eye2_y,15,25,open_color.black)
+arcade.draw_ellipse_filled(eye1_x,eye1_y,25,40,open_color.black)
+arcade.draw_ellipse_filled(eye2_x,eye2_y,25,40,open_color.black)
 arcade.draw_circle_filled(catch1_x,catch1_y,3,open_color.gray_2)
 arcade.draw_circle_filled(catch2_x,catch2_y,3,open_color.gray_2)
 
 #(x,y,width,height,color,start_degrees,end_degrees,border_thickness)
-arcade.draw_arc_outline(smile_x,smile_y,60,50,open_color.black,190,350,4)
+arcade.draw_arc_outline(smile_x,smile_y,90,70,open_color.black,190,350,4)
 
 
 
